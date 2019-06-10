29316ff3f14cda8e19a4280beb4808d655d86fc3
Jason Leach <jlspursfan@gmail.com>
2019-06-10 16:29:25 -0700

    Boating Test Commit - JL

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
 class Instructor
 
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(stud, test_name)
+    BoatingTest.all.each do |test|
+      if (test.student == stud) && (test.name == test_name)
+        test.status = 'passed'
+      else
+        BoatingTest.new(student, name, status = 'passed', self)
+      end
+    end
+  end
+
+  def fail_student(stud, test_name)
+    BoatingTest.all.each do |test|
+      if (test.student == stud) && (test.name == test_name)
+        test.status = 'failed'
+      else
+        BoatingTest.new(student, name, status = 'failed', self)
+      end
+    end
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 class Student
+
+  @@all = []
+  attr_accessor :first_name
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(name, status, instructor)
+    BoatingTest.new(self, name, status, instructor)
+  end
+
+  def self.find_student(name)
+    self.all.select do |student|
+      student.first_name == name
+    end
+  end
+
+  def grade_percentage
+    tests = BoatingTest.all.select do |test|
+      test.student == self
+    end
+
+    passed_tests = BoatingTest.all.select do |test|
+      (test.student == self) && (test.status == 'passed')
+    end
+
+    x = passed_tests.length
+    y = tests.length.to_f
+    return x/y
+  end
 
 end
